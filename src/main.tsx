1ee1a0378bc571629d0ff75c1eee09c1603538ab
Pedro Santos <realpedrosantos@icloud.com>
2026-08-08 19:32:15 -0300

    fix: apply live pre-player alignment override

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
+import './live-preview-fix.css'
 
 // Register Samsung Tizen remote keys
 try {
